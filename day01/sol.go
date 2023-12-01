2519115027575557e53c680a835f1f8cc07923fe
Martin Morin <morin@iternio.com>
2023-12-01 22:50:23 +0100

    day 1

--- day01/sol.go
+++ day01/sol.go
@@ -1,12 +1,95 @@
 package day01
 
-import "fmt"
+import (
+	"fmt"
+	// "os"
+	"strings"
+	"unicode"
+	"embed"
+)
 
-func Prob1() {
-	fmt.Println("problem 1")
+//go:embed input.txt input_test.txt
+var efs embed.FS
+
+func read_lines(f string) []string {
+	bytes, err := efs.ReadFile(f)
+    if err != nil {
+        // fmt.Print(err)
+		panic(err)
+    }
+
+	s := string(bytes)
+	lines := strings.Split(s, "\n")
+	return lines[:len(lines)-1]
+}
+
+func Prob1() int {
+	lines := read_lines("input.txt")
+
+	sum := 0
+	for _, line := range lines {
+		digit := -1
+		for _, r := range line {
+			if unicode.IsDigit(r) {
+				cur_digit := int(r - '0')
+				if digit == -1 {
+					sum = sum + 10*cur_digit
+				}
+				digit = cur_digit
+			}
+		}
+		sum = sum + digit
+	}
+
+	fmt.Println(sum)
+	return sum
 
 }
 
-func Prob2() {
-	fmt.Println("problem 2")
+func Prob2() int {
+	lines := read_lines("input.txt")
+
+	digit_words := [...]string{
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+	}
+
+	sum := 0
+	for _, line := range lines {
+		first_digit := -1
+		second_digit := -1
+
+		for i, r := range line {
+			found_digit := -1
+
+			if unicode.IsDigit(r) {
+				found_digit = int(r - '0')
+			} else {
+				for d, w := range digit_words {
+					if i+len(w) <= len(line) && w == line[i:i+len(w)] {
+						found_digit = d + 1
+						break
+					}
+				}
+			}
+
+			if found_digit >= 0 {
+				if first_digit < 0 {
+					first_digit = found_digit
+				}
+				second_digit = found_digit
+			}
+		}
+		sum = sum + 10*first_digit + second_digit
+	}
+
+	fmt.Println(sum)
+	return sum
 }
--- day01/sol_test.go
+++ day01/sol_test.go
@@ -3,9 +3,17 @@ package day01
 import "testing"
 
 func Test1(t *testing.T) {
-	Prob1()
+	res := Prob1()
+	correct := 55108
+	if res != correct {
+		t.Errorf("Wrong result %d, should be %d", res, correct)
+	}
 }
 
 func Test2(t *testing.T) {
-	Prob2()
+	res := Prob2()
+	correct := 56324
+	if res != correct {
+		t.Errorf("Wrong result %d, should be %d", res, correct)
+	}
 }
