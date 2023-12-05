fd3d1238b2acfd21dfcb62135241011a6afd8965
Martin Morin <morin@iternio.com>
2023-12-06 00:33:38 +0100

    "fix" "slow" day 1

--- day01/sol.go
+++ day01/sol.go
@@ -2,92 +2,104 @@ package day01
 
 import (
 	"fmt"
-	// "os"
-	"strings"
-	"unicode"
-	"embed"
+	_ "embed"
 )
 
-//go:embed input.txt input_test.txt
-var efs embed.FS
+//go:embed input.txt
+var input []byte
 
-func read_lines(f string) []string {
-	bytes, err := efs.ReadFile(f)
-    if err != nil {
-        // fmt.Print(err)
-		panic(err)
-    }
+var digit_words [9]string = [9]string{
+	"one",
+	"two",
+	"three",
+	"four",
+	"five",
+	"six",
+	"seven",
+	"eight",
+	"nine",
+}
 
-	s := string(bytes)
-	lines := strings.Split(s, "\n")
-	return lines[:len(lines)-1]
+func try_parse_digit(buf []byte, i int, words bool) (int, int, bool) {
+	val := 0
+	found := false
+	if '0' <= buf[i] && buf[i] <= '9' {
+		found = true
+		val = int(buf[i] - '0')
+	}
+
+	for wi := 0; words && !found && wi < len(digit_words); wi++ {
+		word := digit_words[wi]
+		sub := 0
+		for i+sub < len(buf) && sub < len(word) && buf[i+sub] == word[sub] {
+			sub++
+		}
+		found = sub == len(word)
+		if found {
+			val = wi + 1
+			i += sub - 2
+		}
+	}
+
+	i++
+	return val, i, found
 }
 
 func Prob1() int {
-	lines := read_lines("input.txt")
+	buf := input
 
 	sum := 0
-	for _, line := range lines {
-		digit := -1
-		for _, r := range line {
-			if unicode.IsDigit(r) {
-				cur_digit := int(r - '0')
-				if digit == -1 {
-					sum = sum + 10*cur_digit
-				}
-				digit = cur_digit
+	first := -1
+	last := -1
+	i := 0
+	for i < len(buf) {
+		if buf[i] == '\n' || i+1 == len(buf) {
+			sum += 10*first + last
+			i++
+			first = -1
+			last = -1
+			continue
+		}
+
+		val, i_new, found := try_parse_digit(buf, i, false)
+		i = i_new
+		if found {
+			last = val
+			if first < 0 {
+				first = val
 			}
 		}
-		sum = sum + digit
 	}
 
 	fmt.Println(sum)
 	return sum
-
 }
 
 func Prob2() int {
-	lines := read_lines("input.txt")
-
-	digit_words := [...]string{
-		"one",
-		"two",
-		"three",
-		"four",
-		"five",
-		"six",
-		"seven",
-		"eight",
-		"nine",
-	}
+	buf := input
 
 	sum := 0
-	for _, line := range lines {
-		first_digit := -1
-		second_digit := -1
+	first := -1
+	last := -1
+	i := 0
+	for i < len(buf) {
+		if buf[i] == '\n' || i+1 == len(buf) {
+			cal := 10*first + last
+			sum += cal
+			i++
+			first = -1
+			last = -1
+			continue
+		}
 
-		for i, r := range line {
-			found_digit := -1
-
-			if unicode.IsDigit(r) {
-				found_digit = int(r - '0')
-			} else {
-				for d, w := range digit_words {
-					if i+len(w) <= len(line) && w == line[i:i+len(w)] {
-						found_digit = d + 1
-						break
-					}
-				}
-			}
-
-			if found_digit >= 0 {
-				if first_digit < 0 {
-					first_digit = found_digit
-				}
-				second_digit = found_digit
+		val, i_new, found := try_parse_digit(buf, i, true)
+		i = i_new
+		if found {
+			last = val
+			if first < 0 {
+				first = val
 			}
 		}
-		sum = sum + 10*first_digit + second_digit
 	}
 
 	fmt.Println(sum)
