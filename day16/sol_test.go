cbb3332a6daad85c685b9a4765e43e8570bab474
Martin Morin <morin@iternio.com>
2023-12-17 21:08:38 +0100

    fix day 16 test

--- day16/sol_test.go
+++ day16/sol_test.go
@@ -14,7 +14,7 @@ func Test1(t *testing.T) {
 
 func Test2(t *testing.T) {
 	res := Prob2()
-	correct := 0
+	correct := 7315
 	if res != correct {
 		t.Errorf("Wrong result %d, should be %d", res, correct)
 	}
